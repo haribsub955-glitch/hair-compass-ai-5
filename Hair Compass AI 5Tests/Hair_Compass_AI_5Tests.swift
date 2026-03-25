8d04ff41d1cf909e899acb89d6464fdcb06a84d1
haribsub955-glitch <haribsub955@gmail.com>
2026-03-25 14:43:10 +0400

    Initial Commit

---
+++ Hair Compass AI 5Tests/Hair_Compass_AI_5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Hair_Compass_AI_5Tests.swift
+//  Hair Compass AI 5Tests
+//
+//  Created by Harib Azri on 25/03/2026.
+//
+
+import Testing
+@testable import Hair_Compass_AI_5
+
+struct Hair_Compass_AI_5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Hair Compass AI 5UITests/Hair_Compass_AI_5UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Hair_Compass_AI_5UITestsLaunchTests.swift
+//  Hair Compass AI 5UITests
+//
+//  Created by Harib Azri on 25/03/2026.
+//
+
+import XCTest
+
+final class Hair_Compass_AI_5UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
